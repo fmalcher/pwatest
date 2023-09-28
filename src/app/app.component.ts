98f022930833755c1b734fdeb8915ae0ed77c83f
Ferdinand Malcher <ferdinand@malcher.media>
2023-09-28 12:42:37 +0200

    swupdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'pwatest';
+
+  constructor(private swUpdate: SwUpdate) {
+    this.swUpdate.versionUpdates.pipe(filter(e => e.type === 'VERSION_READY')).subscribe(() => {
+      if (confirm('Es ist eine neue Version der Seite verfügbar. Installieren?')) {
+        window.location.reload();
+      }
+    });
+  }
 }
